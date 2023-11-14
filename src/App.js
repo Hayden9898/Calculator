25f403576d4c857fc2f914081181974dc663f8cb
Hayden9898 <haydenc9898@gmail.om>
2023-11-14 00:42:33 -0500

    added calculator buttons

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+// import logo from './logo.svg';
+// import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+return (
+    <div className="calculator-grid">
+        <div className="output"> {/* for upper box of the calculator */}
+            <div className="previous-operand"></div>
+            <div className="current-operand"></div> {/*current number being inputed */}
+                
+        </div>
+        <button className="span-two">AC</button> {/*called span 2 as the clear button takes 2 columns */}
+        <button>Del</button> 
+        <button>1</button> 
+        <button>2</button> 
+        <button>3</button> 
+        <button>*</button> {/*creating each row and button */}
+        <button>4</button> 
+        <button>5</button> 
+        <button>6</button> 
+        <button>+</button> 
+        <button>7</button> 
+        <button>8</button> 
+        <button>9</button> 
+        <button>-</button> 
+        <button>.</button> 
+        <button>0</button> 
+        <button className="span-two">=</button> 
     </div>
-  );
+
+)
 }
 
 export default App;
